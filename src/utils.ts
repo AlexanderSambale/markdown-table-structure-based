43d5104e978e40747007154daa5bfd4a1a1cf328
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-02-20 14:30:26 +0100

    Add case in create table Add case each cell is on a newline, ordered by first row, second and so on

--- src/utils.ts
+++ src/utils.ts
@@ -5,22 +5,35 @@ export function create(input: string, columnsNumber: number): string {
   table = input.trim();
   const multipleNewLines = /\n\n+/;
   const columns = table.split(multipleNewLines);
+  let headerRow = '';
+  for(let index = 0; index < columnsNumber; index++) {
+    headerRow = headerRow + ('|:---');
+  }
+  let rows: string[] = [];
   if(columns.length === columnsNumber) {
     // columns are already structured by newline
-    let rows: string[] = [];
     for(let index = 0; index < columns[0].split(/\n/).length; index++) {
       rows.push('');
     }
     columns.forEach(column => column.split(/\n/).forEach((rowCell, index) => rows[index] = rows[index] + "|" + rowCell));
-    let headerRow = '';
-    for(let index = 0; index < columnsNumber; index++) {
-      headerRow = headerRow + ('|:---');
+  } else if (columns.length === 1){
+    // each cell is on a newline, ordered by first row, second and so on
+    const cells = table.split('\n');
+    let row: string;
+    for(let rowIndex = 0; rowIndex*columnsNumber < cells.length; rowIndex++) {
+      row = '';
+      for(let columnIndex = 0; columnIndex < columnsNumber; columnIndex++){
+        if(rowIndex*columnsNumber+columnIndex < cells.length) {
+          row = row + cells[rowIndex*columnsNumber+columnIndex] + '|';
+        } else {
+          row = row + '|';
+        }
+      }
+      rows.push(row);
     }
-    rows.splice(1, 0, headerRow);
-    table = rows.join('\n');
-  } else {
-    // columns are already structured by newline
   }
+  rows.splice(1, 0, headerRow);
+  table = rows.join('\n');
   table = formatTable(table);
   return table; 
 }
