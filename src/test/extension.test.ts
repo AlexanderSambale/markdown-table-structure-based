bcff16a49b6c675702ebaca69f563185fb406ccc
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-02-20 14:04:47 +0100

    Remove Message from tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -2,14 +2,11 @@ import { strictEqual } from 'assert';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import { window } from 'vscode';
 import { clean, create } from '../utils';
 import { formatTable } from '../utils_extern';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
-	window.showInformationMessage('Start all tests.');
-
 	test('Create table with 4 columns', () => {
 		const input = clean(`
 		Arbeitszeit
