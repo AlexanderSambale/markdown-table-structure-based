da95a93d798051676be696baa9460bbf6d0f6312
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-02-20 12:01:35 +0100

    Use clean from utils in tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -3,7 +3,7 @@ import { strictEqual } from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import { window } from 'vscode';
-import { create } from '../utils';
+import { clean, create } from '../utils';
 import { formatTable } from '../utils_extern';
 // import * as myExtension from '../../extension';
 
