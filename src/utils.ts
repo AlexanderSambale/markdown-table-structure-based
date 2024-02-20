5042ea89bc9edd10c581eedac3e69ec5860dd0f5
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-02-20 14:06:12 +0100

    Update create function case columns separated by multiple new lines

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,27 @@
 import { formatTable } from "./utils_extern";
 
-export function create(input: string, columns: number): string {
+export function create(input: string, columnsNumber: number): string {
   let table = '';
-  table = formatTable(input);
+  table = input.trim();
+  const multipleNewLines = /\n\n+/;
+  const columns = table.split(multipleNewLines);
+  if(columns.length === columnsNumber) {
+    // columns are already structured by newline
+    let rows: string[] = [];
+    for(let index = 0; index < columns[0].split(/\n/).length; index++) {
+      rows.push('');
+    }
+    columns.forEach(column => column.split(/\n/).forEach((rowCell, index) => rows[index] = rows[index] + "|" + rowCell));
+    let headerRow = '';
+    for(let index = 0; index < columnsNumber; index++) {
+      headerRow = headerRow + ('|:---');
+    }
+    rows.splice(1, 0, headerRow);
+    table = rows.join('\n');
+  } else {
+    // columns are already structured by newline
+  }
+  table = formatTable(table);
   return table; 
 }
 
