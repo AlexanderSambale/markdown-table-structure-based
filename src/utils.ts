8b6c58167b5a89dfe90f2bd7aed0e42fbb7e7ed8
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-02-20 11:58:52 +0100

    Use formatTable in create function

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,7 @@
+import { formatTable } from "./utils_extern";
+
 export function create(input: string, columns: number): string {
   let table = '';
+  table = formatTable(input);
   return table; 
 }
