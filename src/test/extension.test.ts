a82a7800b5444a7c006dc6a50d78cf68a29c536f
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-02-20 11:01:21 +0100

    Add 2 tests for create table command

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -8,8 +8,38 @@ import * as vscode from 'vscode';
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('Create table with 4 columns', () => {
+		const input = `Arbeitszeit
+		12 Min.
+		
+		Fertig in
+		12 Min.
+		
+		Kalorien
+		53
+		
+		Level
+		Einfach`;
+		const expected = `|Arbeitszeit |Fertig in |Kalorien|Level|
+		| :-- | :-- | :-- | :-- |
+		|12 Min. |12 Min. |53|Einfach`
+		assert.strictEqual(create(input, 4), expected);
+	});
+
+	test('Create table with 2 columns', () => {
+		const input = `Zutaten
+		Menge
+		Haferflocken
+		2 Esslöffel / 15 g
+		Wasser
+		0.4 Liter / 400 g
+		Salz
+		1 Prise/n / 1 g`;
+		const expected = `|Zutaten| Menge|
+		| :-- | :-- |
+		|Haferflocken| 2 Esslöffel / 15 g|
+		|Wasser|0.4 Liter / 400 g|
+		|Salz|1 Prise/n / 1 g|`
+		assert.strictEqual(create(input, 2), expected);
 	});
 });
