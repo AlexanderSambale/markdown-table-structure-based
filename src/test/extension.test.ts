0df83a612651a064d0425e8f88a88675d4903959
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-02-21 13:36:25 +0100

    Add test case for preformated and update not formated concat tables

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -97,23 +97,47 @@ suite('Extension Test Suite', () => {
 	test('Concat 2 tables', () => {
 		const input = clean(`
 		| Zutaten      | Menge              |
-		|:-------------|:-------------------|
+		| :----------- | :----------------- |
 		| Haferflocken | 2 Esslöffel / 15 g |
 		| Wasser       | 0.4 Liter / 400 g  |
 		| Salz         | 1 Prise/n / 1 g    |
 
-		| Z      | M              |
-		|:-------------|:-------------------|
-		| H | 2  |
-		| W       | 0.4   |
-		| S         | 1  |		
+		| Z   | M   |
+		| :-- | :-- |
+		| H   | 2   |
+		| W   | 0.4 |
+		| S   | 1   |		
 		`);
 		const expected = clean(`
-		| Zutaten      | Menge              | Z      | M              |
-		|:-------------|:-------------------|:-------------|:-------------------|
-		| Haferflocken | 2 Esslöffel / 15 g | H | 2  |
-		| Wasser       | 0.4 Liter / 400 g  | W       | 0.4   |
-		| Salz         | 1 Prise/n / 1 g    | S         | 1  |
+		| Zutaten      | Menge              | Z   | M   |
+		| :----------- | :----------------- | :-- | :-- |
+		| Haferflocken | 2 Esslöffel / 15 g | H   | 2   |
+		| Wasser       | 0.4 Liter / 400 g  | W   | 0.4 |
+		| Salz         | 1 Prise/n / 1 g    | S   | 1   |
+		`);
+		strictEqual(concat(input), expected);
+	});
+
+	test('Concat 2 tables not formated', () => {
+		const input = clean(`
+		| Zutaten | Menge |
+		| :-- | :-- |
+		| Haferflocken | 2 Esslöffel / 15 g |
+		| Wasser | 0.4 Liter / 400 g |
+		| Salz | 1 Prise/n / 1 g |
+
+		| Z|M   |
+		|:-- | :--|
+		| H |2   |
+		| W  | 0.4 |
+		| S   |1   |		
+		`);
+		const expected = clean(`
+		| Zutaten      | Menge              | Z   | M   |
+		| :----------- | :----------------- | :-- | :-- |
+		| Haferflocken | 2 Esslöffel / 15 g | H   | 2   |
+		| Wasser       | 0.4 Liter / 400 g  | W   | 0.4 |
+		| Salz         | 1 Prise/n / 1 g    | S   | 1   |
 		`);
 		strictEqual(concat(input), expected);
 	});
